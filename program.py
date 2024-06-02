91d1e4939233f4e40f11abf1eed2c3b2b0a2d054
jgrodzki <240675@edu.p.lodz.pl>
2024-06-02 17:21:51 +0200

    ptp master

--- program.py
+++ program.py
@@ -3,6 +3,7 @@
 import subprocess
 import dbus
 import epd2in7
+from datetime import datetime, timezone
 from signal import pause, signal, SIGTERM, SIGINT
 from PIL import Image, ImageDraw, ImageFont
 from gpiozero import Button
@@ -10,11 +11,13 @@ from gpiozero import Button
 font = ImageFont.truetype("/usr/share/fonts/truetype/DejaVuSansMono.ttf")
 refresh_button = Button(5)
 restart_button = Button(6)
-dhcp_button = Button(13)
-ptp_button = Button(19)
+view_switch_button = Button(13)
+server_button = Button(19)
 system_bus = dbus.SystemBus()
 epd = epd2in7.EPD()
-current_view = "dhcp"
+ptp_daemon = None
+ptp_master_active = False
+current_view = "ptp"
 
 
 def show_image(image):
@@ -33,6 +36,26 @@ def end_program(signum, frame):
     epd.sleep()
     epd2in7.epdconfig.module_exit(cleanup=True)
     exit()
+
+
+def start_ptp_slave():
+    global ptp_daemon
+    if ptp_daemon:
+        ptp_daemon.terminate()
+    ptp_daemon = subprocess.Popen(["ptp4l", "-S", "-i", "eth0", "-s"])
+
+
+def start_ptp_master():
+    global ptp_daemon
+    if ptp_daemon:
+        ptp_daemon.terminate()
+    ptp_daemon = subprocess.Popen(["ptp4l", "-S", "-i", "eth0"])
+    while True:
+        result = subprocess.run(["pmc", "-u", "-b", "0", "SET PRIORITY1 0"], capture_output=True, text=True)
+        if result.returncode == 0:
+            for line in result.stdout.split('\n'):
+                if "priority1" in line and line.split()[-1] == "0":
+                    return
 
 
 def get_leases():
@@ -65,34 +88,89 @@ def get_leases():
         if lease_string:
             return lease_string
         else:
-            return "No leases"
+            return "(no leases)"
     except dbus.exceptions.DBusException:
         return "Interface not configured yet"
 
 
-def get_ptp_info():
+def clock_identity_to_mac(clock_identity):
+    parts = clock_identity.strip().split(".")
+    parts = parts[0]+parts[2]
+    return ':'.join(
+        a + b for a, b in zip(parts[::2], parts[1::2]))
+
+
+def get_master():
+    info = {
+        "foreign_master": False,
+        "current_time": None,
+        "current_offset": None,
+        "current_master": None,
+        "master_description": None
+    }
     result = subprocess.run(
-        ["pmc", "-u", "GET TIME_STATUS_NP"], capture_output=True, text=True)
+        ["pmc", "-u", "-b", "0", "GET TIME_STATUS_NP"], capture_output=True, text=True)
     if result.returncode == 0:
         for line in result.stdout.split('\n'):
+            if "gmPresent" in line:
+                if "true" in line:
+                    info.update({"foreign_master": True})
+            if "ingress_time" in line:
+                info.update({"current_time": datetime.fromtimestamp(int(line.split()[-1])/10e8, tz=timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")})
+            if "master_offset" in line:
+                info.update({"current_offset": line.split()[-1]})
             if "gmIdentity" in line:
-                grandmaster_id = line.split()[-1].strip().split(".")
-                grandmaster_id = grandmaster_id[0]+grandmaster_id[2]
-                grandmaster_id = ':'.join(
-                    a + b for a, b in zip(grandmaster_id[::2], grandmaster_id[1::2]))
-                return f"Grandmaster: {grandmaster_id}"
+                info.update(
+                    {"current_master": clock_identity_to_mac(line.split()[-1])})
     else:
-        return "Failed to get data"
+        return None
+    if info["foreign_master"] and "current_master":
+        result = subprocess.run(
+            ["pmc", "-u", "GET CLOCK_DESCRIPTION"], capture_output=True, text=True)
+        if result.returncode == 0:
+            master_found = False
+            for line in result.stdout.split('\n'):
+                if not master_found and "physicalAddress" in line and info["current_master"] in line:
+                    master_found = True
+                if "productDescription" in line:
+                    info.update({"master_description": line.split()[-1]})
+                    break
+        else:
+            return None
+    return info
 
 
-def draw_labels(image):
+# def get_ptp():
+#     response = ""
+#     mac_found = False
+#     result = subprocess.run(
+#         ["pmc", "-u", "GET TIME_STATUS_NP"], capture_output=True, text=True)
+#     if result.returncode == 0:
+#         for line in result.stdout.split('\n'):
+#             if re.match(r"^\t[a-f\d]{6}.[a-f\d]{4}.[a-f\d]{6}-\d", line):
+#                 mac = clock_identity_to_mac(line.split("-")[0])
+#                 mac_found = True
+#                 response += mac
+#             if "master_offset" in line:
+#                 if mac_found:
+#                     mac_found = False
+#                     response += " Offset: "+line.split()[-1]+"\n"
+#         if not response:
+#             return "(no clients)"
+#         else:
+#             return response
+#     else:
+#         return "Failed to get data"
+
+
+def draw_labels(image, label1, label2, label3, label4):
     # 264 × 176
     button_labels = Image.new("1", (176, font.size+10), 255)
     button_labels_draw = ImageDraw.Draw(button_labels)
-    button_labels_draw.text((22, 5), "Refresh", font=font, anchor="ma")
-    button_labels_draw.text((66, 5), "Reboot", font=font, anchor="ma")
-    button_labels_draw.text((110, 5), "DHCP", font=font, anchor="ma")
-    button_labels_draw.text((154, 5), "PTP", font=font, anchor="ma")
+    button_labels_draw.text((22, 5), label1, font=font, anchor="ma")
+    button_labels_draw.text((66, 5), label2, font=font, anchor="ma")
+    button_labels_draw.text((110, 5), label3, font=font, anchor="ma")
+    button_labels_draw.text((154, 5), label4, font=font, anchor="ma")
     button_labels_draw.rectangle([0, 0, 44, font.size+10])
     button_labels_draw.rectangle([44, 0, 88, font.size+10])
     button_labels_draw.rectangle([88, 0, 132, font.size+10])
@@ -103,7 +181,7 @@ def draw_labels(image):
 
 def show_dhcp():
     image = Image.new("1", (epd.height, epd.width), 255)
-    draw_labels(image)
+    draw_labels(image, "Refresh", "Reboot", "PTP", "")
     draw = ImageDraw.Draw(image)
     draw.text((font.size+20, 10), "DHCP Info:\n"+get_leases(), font=font)
     show_image(image)
@@ -111,9 +189,21 @@ def show_dhcp():
 
 def show_ptp():
     image = Image.new("1", (epd.height, epd.width), 255)
-    draw_labels(image)
+    if ptp_master_active:
+        draw_labels(image, "Refresh", "Reboot", "DHCP", "Slave")
+    else:
+        draw_labels(image, "Refresh", "Reboot", "DHCP", "Master")
     draw = ImageDraw.Draw(image)
-    draw.text((font.size+20, 10), "PTP Info:\n"+get_ptp_info(), font=font)
+    info = get_master()
+    if ptp_master_active:
+        response = f"Working as PTP master:\nMy MAC:\n{info['current_master']}"
+    else:
+        response = "Working as PTP slave:\n"
+        if info["foreign_master"]:
+            response += f"Master MAC:\n{info['current_master']}\nMaster description:\n{info['master_description']}\nCurrent time:\n{info['current_time']}\nCurrent offset:\n{info['current_offset']}"
+        else:
+            response += "(No foreign master found)"
+    draw.text((font.size+20, 10), response, font=font)
     show_image(image)
 
 
@@ -124,16 +214,33 @@ def refresh():
         show_ptp()
 
 
-def set_view(view):
+def switch_view():
     global current_view
-    current_view = view
+    if current_view == "dhcp":
+        server_button.when_pressed = toggle_ptp_master
+        current_view = "ptp"
+    elif current_view == "ptp":
+        server_button.when_pressed = None
+        current_view = "dhcp"
     refresh()
 
 
+def toggle_ptp_master():
+    global ptp_master_active
+    if ptp_master_active:
+        start_ptp_slave()
+        ptp_master_active = False
+    else:
+        start_ptp_master()
+        ptp_master_active = True
+    refresh()
+
+
+start_ptp_slave()
 restart_button.when_pressed = restart
 refresh_button.when_pressed = refresh
-dhcp_button.when_pressed = lambda: set_view("dhcp")
-ptp_button.when_pressed = lambda: set_view("ptp")
+view_switch_button.when_pressed = switch_view
+server_button.when_pressed = toggle_ptp_master
 signal(SIGTERM, end_program)
 signal(SIGINT, end_program)
 refresh()
